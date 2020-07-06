edc8dff55ed02ebefeef1e125ffe4be6a41fda17
hrupesh <rcashok0@gmail.com>
2020-07-06 22:42:24 +0530

    Starting....

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -4,17 +4,17 @@ import BlogContext from "../context/BlogContext";
 import { FlatList } from "react-native-gesture-handler";
 
 export default function IndexScreen() {
-  const value = useContext(BlogContext);
-  console.log(value);
+  const blogs = useContext(BlogContext);
+  console.log(blogs);
 
   return (
     <View>
       <Text>Index Screen works! </Text>
-      <FlatList 
-        data={value}
-        keyExtractor={(blog) => blog.Title }
-        renderItem={(item) =>{
-            return <Text>{item}</Text>
+      <FlatList
+        data={blogs}
+        keyExtractor={(blog) => blog.Title}
+        renderItem={({item}) => {
+        return <Text>{item.Title} : {item.Body}</Text>;
         }}
       />
     </View>
