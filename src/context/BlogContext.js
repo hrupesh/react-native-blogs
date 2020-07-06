a6adbd79ce0ad945440e58b954cd7a3a085803f7
hrupesh <rcashok0@gmail.com>
2020-07-07 00:41:07 +0530

    Starting....

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,14 +3,14 @@ import React, { useState } from "react";
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
-  const blogPosts = [
-    { Title: "Blog 1", Body: "Body for blog 1" },
-    { Title: "Blog 2", Body: "Body for blog 2" },
-    { Title: "Blog 3", Body: "Body for blog 3" },
-    { Title: "Blog 4", Body: "Body for blog 4" },
-  ];
+//   const blogPosts = [
+//     { Title: "Blog 1", Body: "Body for blog 1" },
+//     { Title: "Blog 2", Body: "Body for blog 2" },
+//     { Title: "Blog 3", Body: "Body for blog 3" },
+//     { Title: "Blog 4", Body: "Body for blog 4" },
+//   ];
 
-  const [blogs, setBlogs] = useState(blogPosts);
+  const [blogs, setBlogs] = useState([]);
 
   const addBlog = () => {
     setBlogs([
