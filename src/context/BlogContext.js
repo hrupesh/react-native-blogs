06ef3a0038d4d894bcccd655f504670dce0054e1
hrupesh <rcashok0@gmail.com>
2020-07-06 22:17:30 +0530

    Starting....

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,7 +3,13 @@ import React from "react";
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
-  return <BlogContext.Provider value={4}>{children}</BlogContext.Provider>;
+  const blogs = [
+    { Title: "Blog 1", Body: "Body for blog 1" },
+    { Title: "Blog 2", Body: "Body for blog 2" },
+    { Title: "Blog 3", Body: "Body for blog 3" },
+    { Title: "Blog 4", Body: "Body for blog 4" },
+  ];
+  return <BlogContext.Provider value={blogs}>{children}</BlogContext.Provider>;
 };
 
 export default BlogContext;
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -4,11 +4,12 @@ import BlogContext from "../context/BlogContext";
 
 export default function IndexScreen() {
   const value = useContext(BlogContext);
+  console.log(value);
 
   return (
     <View>
       <Text>Index Screen works! </Text>
-      <Text>Value = {value}</Text>
+      <Text>Value = </Text>
     </View>
   );
 }
