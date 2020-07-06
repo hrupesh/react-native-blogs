fa8cb55358092f3507ba05491ce803c675fb3da5
hrupesh <rcashok0@gmail.com>
2020-07-07 00:16:17 +0530

    Starting....

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
-  const blogs = [
+  const blogPosts = [
     { Title: "Blog 1", Body: "Body for blog 1" },
     { Title: "Blog 2", Body: "Body for blog 2" },
     { Title: "Blog 3", Body: "Body for blog 3" },
     { Title: "Blog 4", Body: "Body for blog 4" },
   ];
+
+  const [blogs, setBlogs] = useState(blogPosts);
+
+  const addBlog = () => {
+    setBlogs([
+      ...blogPosts,
+      {
+        Title: `Blog ${blogPosts.length + 1}`,
+        Body: `Body for blog ${blogPosts.length + 1}`,
+      },
+    ]);
+  };
+
   return <BlogContext.Provider value={blogs}>{children}</BlogContext.Provider>;
 };
 
