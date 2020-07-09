5cfe2b14be13d88055b945465e43b59b647e28f2
hrupesh <rcashok0@gmail.com>
2020-07-10 04:43:27 +0530

    Added Context

--- src/screens/ViewBlog.js
+++ src/screens/ViewBlog.js
@@ -2,18 +2,35 @@ import React, { useContext } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import BlogContext from "../context/BlogContext";
 
-export default function ViewBlog({ route, navigation }) {
+export default function ViewBlog({ navigation }) {
   // const { data } = useContext(BlogContext);
 
-  //   const { blog } = route.params;
-  console.log(navigation);
-  console.log(route);
+  const { blog } = navigation.state.params;
+  console.log(blog);
 
   return (
-    <View>
-      <Text>hi!</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>{blog.Title}</Text>
+      <Text style={styles.body}>{blog.Body}</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#444",
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+    letterSpacing: 1,
+    paddingBottom: 10,
+    color: "white",
+  },
+  body: {
+    color: "white",
+    fontSize: 16,
+    letterSpacing: 0.5,
+  },
+});
