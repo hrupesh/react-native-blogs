21a7b8be57f1a55ad41a2c0f83a9777715d1d709
hrupesh <rcashok0@gmail.com>
2020-07-12 21:27:01 +0530

    Commits for 12/07/2020

--- src/screens/ViewBlog.js
+++ src/screens/ViewBlog.js
@@ -6,7 +6,6 @@ export default function ViewBlog({ navigation }) {
   const { data, deleteBlogPost } = useContext(BlogContext);
 
   const { blog } = navigation.state.params;
-  console.log("sadffd");
 
   const deletethisBlog = () => {
     deleteBlogPost(blog.id);
