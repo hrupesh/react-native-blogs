f3587bfbe1a922925eed49d24674572251c12362
hrupesh <rcashok0@gmail.com>
2020-07-13 19:40:18 +0530

    Commits for 13/07/2020

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,12 +1,16 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useContext } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import BlogContext from "../context/BlogContext";
 
 export default function CreateScreen() {
-    return (
-        <View>
-            <Text>Create Screen Works!</Text>
-        </View>
-    )
+  const { data, addBlogPost } = useContext(BlogContext);
+  console.log(data);
+
+  return (
+    <View>
+      <Text>Create Screen Works!</Text>
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
